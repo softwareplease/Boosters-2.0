ada9a7ebee56016b0015fe0cb815f91b56c4ef08
softwareplease <softwareplease.io@gmail.com>
2016-01-16 13:17:12 -0500

    Removed site.js because it is unnecessary with aurelia, added home content for the night at the races, removed jumbotron from volunteer view and replaced it with typical page header.

--- src/Boosters 2.0/wwwroot/src/home/home.js
+++ src/Boosters 2.0/wwwroot/src/home/home.js
@@ -1,7 +1,14 @@
-﻿import $ from 'bootstrap';
+﻿import {inject} from 'aurelia-framework';
+import {Validation} from 'aurelia-validation';
+import $ from 'bootstrap';
 
+@inject(Validation)
 export class Home {
-    constructor () {
-        $('#carousel').carousel();
+    constructor (validation) {
+        this.validation = validation;
+        //$('#carousel').carousel();
+
+        //TODO - validation for horses, Add social media icons, email list functionality, horses controls make 50% width
+
     }
 }
