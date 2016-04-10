6cf27b5a438605a0adc2e215ac9ea575aa633ebb
softwareplease <softwareplease.io@gmail.com>
2016-04-10 00:40:37 -0400

    Added new links for the member paypal buttons so they work correctly.

--- src/Boosters 2.0/wwwroot/src/member/member.js
+++ src/Boosters 2.0/wwwroot/src/member/member.js
@@ -2,13 +2,13 @@
 
     constructor() {
         this.memberships = [
-            {name: "Basic", icon: '', amount: 15.00, text: '', selected: true, url: 'https://www.paypal.com/us/cgi-bin/webscr?cmd=_flow&SESSION=rm4SNtViqKE1IgQgfnTvBLZ2EceX5B61ip4-TqR9xboHH5GCk1nKJbBvXsq&dispatch=50a222a57771920b6a3d7b606239e4d529b525e0b7e69bf0224adecfb0124e9b61f737ba21b0819826d26d01da88cb4fd8cb90b3a8c529af'},
-            {name: "Friend", icon: 'glyphicon-user', amount: 25.00, text: '', selected: false, url: 'https://www.paypal.com/us/cgi-bin/webscr?cmd=_flow&SESSION=ad4QI1jsK4aKG_2ScjO52t-tTs7Nw7k1QD7d_InkYsPchjBkjyVgfS1uyxa&dispatch=50a222a57771920b6a3d7b606239e4d529b525e0b7e69bf0224adecfb0124e9b61f737ba21b0819826d26d01da88cb4fd8cb90b3a8c529af'},
-            {name: "Partner", icon: 'glyphicon-heart', amount: 50.00, text: '', selected: false, url: 'https://www.paypal.com/us/cgi-bin/webscr?cmd=_flow&SESSION=MLYQjZYyImltFBIFn0uc_XmTg4o58DgCC7nTwQuIEUYIY6tsQKLSaMMkApS&dispatch=50a222a57771920b6a3d7b606239e4d529b525e0b7e69bf0224adecfb0124e9b61f737ba21b0819826d26d01da88cb4fd8cb90b3a8c529af'},
-            {name: "Purple", icon: 'glyphicon-stop primary', amount: 75.00, text: 'Includes 2 LHS Event tickets', selected: false, url: 'https://www.paypal.com/us/cgi-bin/webscr?cmd=_flow&SESSION=S9L5PWUyivnuxAcg3PFpCKMw6z_8pCpdalwlbD1OqNqil9kUX3i7dKFwzRG&dispatch=50a222a57771920b6a3d7b606239e4d529b525e0b7e69bf0224adecfb0124e9b61f737ba21b0819826d26d01da88cb4fd8cb90b3a8c529af'},
-            {name: "Gold", icon: 'glyphicon-stop secondary', amount: 100.00, text: 'Includes 4 LHS Event tickets', selected: false, url: 'https://www.paypal.com/us/cgi-bin/webscr?cmd=_flow&SESSION=3dVVF-yCVpSblMpRVM-ni1ofNl9myEqueY9pyq00IMAwv3OpWeJm7xtgF8a&dispatch=50a222a57771920b6a3d7b606239e4d529b525e0b7e69bf0224adecfb0124e9b61f737ba21b0819826d26d01da88cb4fd8cb90b3a8c529af'},
-            {name: "Ranger", icon: 'glyphicon-education', amount: 150.00, text: 'Includes 6 LHS Event tickets', selected: false, url: 'https://www.paypal.com/us/cgi-bin/webscr?cmd=_flow&SESSION=YRWLLsU5WMnzsH2WSnfLq7G5QbojR3rDtMV9SVELJRB1PzDA7HYdLkwVaLu&dispatch=50a222a57771920b6a3d7b606239e4d529b525e0b7e69bf0224adecfb0124e9b61f737ba21b0819826d26d01da88cb4fd8cb90b3a8c529af'},
-            {name: "Star", icon: 'glyphicon-star-empty', amount: 200.00, text: 'Includes 8 LHS Event tickets', selected: false, url: 'https://www.paypal.com/us/cgi-bin/webscr?cmd=_flow&SESSION=oAJK5O0cM-sbpYyOu7efRoG9ydDVngZv6q-C1jTb-Q-1Ej8x9tXn1F5IqVu&dispatch=50a222a57771920b6a3d7b606239e4d529b525e0b7e69bf0224adecfb0124e9b61f737ba21b0819826d26d01da88cb4fd8cb90b3a8c529af'}
+            {name: "Basic", icon: '', amount: 15.00, text: '', selected: true, url: 'https://www.paypal.com/cgi-bin/webscr?cmd=_s-xclick&hosted_button_id=B8GS2NDU4D3KQ'},
+            {name: "Friend", icon: 'glyphicon-user', amount: 25.00, text: '', selected: false, url: 'https://www.paypal.com/cgi-bin/webscr?cmd=_s-xclick&hosted_button_id=XJABLR2DSB9R8'},
+            {name: "Partner", icon: 'glyphicon-heart', amount: 50.00, text: '', selected: false, url: 'https://www.paypal.com/cgi-bin/webscr?cmd=_s-xclick&hosted_button_id=VMH6JEZ7Q6J3E'},
+            {name: "Purple", icon: 'glyphicon-stop primary', amount: 75.00, text: 'Includes 2 LHS Event tickets', selected: false, url: 'https://www.paypal.com/cgi-bin/webscr?cmd=_s-xclick&hosted_button_id=R5T2ZAYSZSU7G'},
+            {name: "Gold", icon: 'glyphicon-stop secondary', amount: 100.00, text: 'Includes 4 LHS Event tickets', selected: false, url: 'https://www.paypal.com/cgi-bin/webscr?cmd=_s-xclick&hosted_button_id=43KH69UYRE3H8'},
+            {name: "Ranger", icon: 'glyphicon-education', amount: 150.00, text: 'Includes 6 LHS Event tickets', selected: false, url: 'https://www.paypal.com/cgi-bin/webscr?cmd=_s-xclick&hosted_button_id=VE2KCKNJNWDMC'},
+            {name: "Star", icon: 'glyphicon-star-empty', amount: 200.00, text: 'Includes 8 LHS Event tickets', selected: false, url: 'https://www.paypal.com/cgi-bin/webscr?cmd=_s-xclick&hosted_button_id=QUDKJ4LGRYR52'}
         ];
         this.currentMembership = this.memberships[0];
     }
