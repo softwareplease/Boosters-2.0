1e096580af7a5ccd4c613476f110baf10d9b0509
softwareplease <softwareplease.io@gmail.com>
2017-01-13 00:22:16 -0500

    Updated membership flyer. Made changes to the board. began putting NATR content onto the home page for 2017. Updated prices on the signage page.

--- src/Boosters 2.0/wwwroot/src/board/board.js
+++ src/Boosters 2.0/wwwroot/src/board/board.js
@@ -9,7 +9,7 @@
             {name: 'Nici Crislip', position: '3rd Vice President of Marketing', email: 'nicicrislip@yahoo.com'},
             {name: 'Charles Hokky', position: 'Treasurer', email: 'cshokky@att.net'},
             {name: 'Renee Mahoney', position: 'Treasurer Fundraisers', email: 'nicholasmahoney@sbcglobal.net'},
-            {name: 'Debbie Riddle', position: 'Recording Secretary', email: 'driddledesign@cox.net'},
+            {name: 'Kate Sala', position: 'Recording Secretary', email: 'k8sala88@gmail.com'},
             {name: 'Ann Byczek', position: 'Corresponding Secretary', email: 'Lkann1@aol.com'},
             {name: 'Lisa Mullen Cimino', position: 'Membership Secretary/Booster Bucks Coordinator', email: 'lmullencimino@sbcglobal.net'}
         ];
@@ -18,7 +18,7 @@
             {name: 'Anne Giffels', position: 'Athletic Director Secretary', email: 'anne.giffels@lakewoodcityschools.org'}
         ];
         this.delegates = [
-            {name: 'Dawn Gerakis', position: 'Ranger Shop Manager', email: 'dawnmcampbell@yahoo.com'},
+            {name: 'Karen Lee', position: 'Ranger Shop Manager', email: 'kmlee@att.net'},
             {name: 'David Schmidt', position: 'Web Developer', email: 'schmidtdavid0707@yahoo.com'}
         ];
     }
--- src/Boosters 2.0/wwwroot/src/home/home.js
+++ src/Boosters 2.0/wwwroot/src/home/home.js
@@ -1,10 +1,50 @@
 ﻿import $ from 'bootstrap';
 
+//export class Home {
+//    constructor () {
+//        //$('#carousel').carousel({
+//        //    interval: 5000
+//        //});
+//        //TODO - Add social media icons, email list functionality
+//    }
+//}
 export class Home {
     constructor () {
-        //$('#carousel').carousel({
-        //    interval: 5000
-        //});
-        //TODO - Add social media icons, email list functionality
+        this.horseInputs = [
+            {horseName: null, ownerName: null}
+        ];
+        this.horseAndOwnerNames;
+        this.horseOptions = [
+            {value: 1, text: '1 $15.00'},
+            {value: 2, text: '2 $30.00'},
+            {value: 3, text: '3 $45.00'},
+            {value: 4, text: '4 $60.00'},
+            {value: 5, text: '5 $75.00'},
+            {value: 6, text: '6 $90.00'},
+            {value: 7, text: '7 $105.00'},
+            {value: 8, text: '8 $120.00'},
+            {value: 9, text: '9 $135.00'},
+            {value: 10, text: '10 $150.00'}
+        ];
+    }
+    select(event) {
+        let amount = Number($("select[name='os0']").val());
+        if(amount > this.horseInputs.length) {
+            for (var i = this.horseInputs.length; i < amount; i++) {
+                this.horseInputs.push({horseName: '', ownerName: ''});
+            }
+        } else if (amount < this.horseInputs.length) {
+            this.horseInputs.splice(amount, this.horseInputs.length);
+        }
+    }
+    submitHorses() {
+        var value = '';
+        for(var i = 0; i < this.horseInputs.length; i++) {
+            value += this.horseInputs[i].ownerName + '-' + this.horseInputs[i].horseName;
+            if (i + 1 != this.horseInputs.length)
+                value += '/';
+        }
+        this.horseAndOwnerNames = value;
+        return true;
     }
 }
