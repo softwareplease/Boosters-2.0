11fbc72652b92466cdc81bbbe461f42572c7aafc
softwareplease <softwareplease.io@gmail.com>
2017-01-22 01:58:58 -0500

    Added car raffle content and pdfs. Added bluebird promise poyfill.

--- src/Boosters 2.0/wwwroot/src/raffle/raffle.js
+++ src/Boosters 2.0/wwwroot/src/raffle/raffle.js
@@ -1,5 +1,34 @@
 ﻿export class Raffle {
     constructor() {
-        
+        this.teams = [
+            'Football',
+            'Cheerleading',
+            'Volleyball',
+            'Golf',
+            'Boys Cross Country',
+            'Girls Cross Country',
+            'Boys Soccer',
+            'Girls Soccer',
+            'Girls Tennis',
+            'Boys Basketball',
+            'Girls Basketball',
+            'Gymnastics',
+            'Wrestling',
+            'Ice Hockey',
+            'Swimming and Diving',
+            'Boys and Girls Indoor Track',
+            'Boys Baseball',
+            'Girls Softball',
+            'Boys Tennis',
+            'Boys Track and Field',
+            'Girls Track and Field',
+            'Boys and Girls Rugby'
+        ];
+        this.selectedTeam = this.teams[0];
     }
+
+    select(e) {
+        this.selectedTeam = e.target.value;
+    }
+
 }
