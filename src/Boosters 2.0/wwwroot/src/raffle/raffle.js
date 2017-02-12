0d225d17314f06d8db754c8984f970d111550166
softwareplease <softwareplease.io@gmail.com>
2017-02-12 12:14:07 -0500

    Added ability to buy multiple car raffle tickets at one time.

--- src/Boosters 2.0/wwwroot/src/raffle/raffle.js
+++ src/Boosters 2.0/wwwroot/src/raffle/raffle.js
@@ -25,11 +25,21 @@
             'Girls Track and Field',
             'Boys and Girls Rugby'
         ];
+        this.tickets = [
+            {value: 1, text: '1 $25.00 USD'},
+            {value: 2, text: '2 $50.00 USD'},
+            {value: 3, text: '3 $75.00 USD'},
+            {value: 4, text: '4 $100.00 USD'}
+        ];
         this.selectedTeam = this.teams[0];
+        this.selectedTickets = this.tickets[0].value;
     }
 
-    select(e) {
+    selectTeam(e) {
         this.selectedTeam = e.target.value;
     }
 
+    selectTickets(e) {
+        this.selectedTickets = e.target.value;
+    }
 }
